2a04531bc834688cc66ce48ef8bafd9cfec061cf
Collins Orighose <collinsefe@yahoo.com>
2025-05-06 22:15:45 +0100

    updated ec2

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,55 @@
 # create an EC2 instance - https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 
-resource "aws_instance" "this" {
-  ami = "ami-0fbbcfb8985f9a341"
-  # count         = 1
+locals {
+  server_name = [
+    "bob",
+    "john",
+    "collins"
+  ]
+}
+
+
+resource "aws_instance" "collins" {
+  ami = "ami-0dfe0f1abee59c78d"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.this.id
   associate_public_ip_address = true
   key_name                    = aws_key_pair.this.key_name
-
+  user_data                   = file("${path.module}/user-data.sh")
 
   tags = {
     Name          = "collins-ec2-instance"
+    Deployed_from = "Terraform"
+  }
+}
+
+
+resource "aws_instance" "bob" {
+  ami = "ami-0dfe0f1abee59c78d"
+  # count = 0
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.this.id
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.this.key_name
+  user_data                   = file("${path.module}/user-data.sh")
+
+  tags = {
+    Name          = "bob-ec2-instance"
+    Deployed_from = "Terraform"
+  }
+}
+
+resource "aws_instance" "john" {
+  ami = "ami-0dfe0f1abee59c78d"
+  # count = 0
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.this.id
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.this.key_name
+  user_data                   = file("${path.module}/user-data.sh")
+
+  tags = {
+    Name          = "john-ec2-instance"
     Deployed_from = "Terraform"
   }
 }
